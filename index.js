1561a5f25f7fdabccfa6625fda32baddc2295bfd
Yoni12ab <yoni12ab@gmail.com>
2021-11-19 15:38:48 +0200

    fix new time watch

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const chromePaths = require("chrome-paths");
     await login(COMPANY_NUMBER, EMPLOYEE_NUMBER, PASSWORD);
     await gotToReports();
     await selectMonthIfNeeded();
+    await page.waitFor(3000);
     await fillMissing();
 
     await browser.close();
